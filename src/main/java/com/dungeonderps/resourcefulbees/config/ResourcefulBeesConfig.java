b1dba42b59d2d86ff375b588669f1b42f61a2cc2
Darkfire <2017BrandonH@gmail.com>
2020-03-30 17:03:36 -0400

    Update ResourcefulBeesConfig.java
    added some content to config files

--- src/main/java/com/dungeonderps/resourcefulbees/config/ResourcefulBeesConfig.java
+++ src/main/java/com/dungeonderps/resourcefulbees/config/ResourcefulBeesConfig.java
@@ -1,35 +1,38 @@
 package com.dungeonderps.resourcefulbees.config;
 
+import com.dungeonderps.resourcefulbees.entity.BeeBuilderEntity;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import jdk.nashorn.internal.parser.JSONParser;
+import net.minecraft.client.renderer.debug.BeeDebugRenderer;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.commons.lang3.tuple.Pair;
 import com.electronwill.nightconfig.toml.TomlFormat;
 import com.electronwill.nightconfig.core.Config;
 
+import java.awt.*;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Map;
 
 public class ResourcefulBeesConfig {
-    public static final Common COMMON;
+    public static class CommonConfig {
+        public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+        public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec COMMON_SPEC;
-
-    static {
-        final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
-        COMMON = specPair.getLeft();
-        COMMON_SPEC = specPair.getRight();
-    }
-
-    public static class Common {
-        public final ForgeConfigSpec.ConfigValue<Config> mobCustomBees;
-
-        Common(final ForgeConfigSpec.Builder builder) {
-            builder.push("common");
-
-            final Map<String, ArrayList<String>>;
-//            this.mobCustomBees = builder.comment()
+        static {
+            BUILDER.comment("Mod options");
+            SPEC = BUILDER.build();
         }
     }
-
+    // might delete
     public static class BeeConfig {
         private ForgeConfigSpec.BooleanValue enabled;
         private ForgeConfigSpec.ConfigValue<ArrayList<Integer>> beeColor;
@@ -38,6 +41,50 @@ public class ResourcefulBeesConfig {
         private ForgeConfigSpec.ConfigValue<String> name;
         private ForgeConfigSpec.ConfigValue<String> resource;
 
+    }
 
+    // setup the mod config folder
+    public static void setup() {
+        Path configPath = FMLPaths.CONFIGDIR.get();
+        Path rbConfigPath = Paths.get(configPath.toAbsolutePath().toString(), "resourcefulbees");
+        // subfolder for bees
+        Path rbBeesPath = Paths.get(rbConfigPath.toAbsolutePath().toString(), "bees");
+        try {
+            Files.createDirectory(rbConfigPath);
+            Files.createDirectory(rbBeesPath);
+        } catch (FileAlreadyExistsException e) {
+            // do nothing
+        } catch (IOException e) {
+            //ResourcefulBeeLogger.logger.error("Failed to create resourcefulbees config directory");
+        }
+        writeDefault();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC, "resourcefulbees/common.toml");
+    }
+
+    // write default bees
+    public static void writeDefault() {
+        // example resource array will add to this
+//        String[] drops
+//        try {
+//
+//        } catch (FileAlreadyExistsException e) {
+//            // do nothing
+//        } catch (IOException e) {
+//            // log thing
+//        }
+    }
+
+    // parse bees here. to be used where bees are registered.
+    public BeeBuilderEntity parseBee(File file) throws FileNotFoundException {
+        Gson gson = new Gson();
+
+        try {
+            // log the reading of the file here
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            return gson.fromJson(reader, BeeBuilderEntity.class);
+        } catch (IOException e) {
+            // log an error happened
+        }
+        return null;
     }
 }
