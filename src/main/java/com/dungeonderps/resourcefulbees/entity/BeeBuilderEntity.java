bff309e3615c0c7f198c55db9bc103e13fcca807
Darkfire <2017BrandonH@gmail.com>
2020-03-31 12:15:21 -0400

    Update BeeBuilderEntity.java
    filled in more of BeeBuilderEntity constructor content

--- src/main/java/com/dungeonderps/resourcefulbees/entity/BeeBuilderEntity.java
+++ src/main/java/com/dungeonderps/resourcefulbees/entity/BeeBuilderEntity.java
@@ -1,17 +1,21 @@
 package com.dungeonderps.resourcefulbees.entity;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.awt.*;
 
 public class BeeBuilderEntity extends BeeEntity {
     String name;
-    Item drop;
+    ItemStack drop;
     Color beeColor, hiveColor;
     Block flower, baseBlock, mutBlock;
 
@@ -20,8 +24,29 @@ public class BeeBuilderEntity extends BeeEntity {
         this.name = name;
         this.beeColor = new Color(beeColor[0], beeColor[1],beeColor[2]);
         this.hiveColor = new Color(hiveColor[0], hiveColor[1], hiveColor[2]);
-        this.drop = new Item();
+        this.drop = new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(drop)));
+        this.baseBlock = Block.getBlockFromItem(ForgeRegistries.ITEMS.getValue(new ResourceLocation(baseBlock)));
+        this.mutBlock = Block.getBlockFromItem(ForgeRegistries.ITEMS.getValue(new ResourceLocation(mutBlock)));
         // create checker for what they passed in for flower
+        // single flower
+        // tag
+        // all
+        // small only
+        // tall only
+        switch (flower) {
+            case "all":
+
+            case "small":
+
+            case "tall":
+
+            default:
+                if (flower.charAt(0) == '#') {
+                    // do something
+                } else {
+                    // do something
+                }
+        }
     }
     // CONSTANT FOR EVERY ENTITY
 
