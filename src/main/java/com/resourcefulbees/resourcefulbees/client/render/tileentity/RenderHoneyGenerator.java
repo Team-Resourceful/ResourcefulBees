1947ffb791d33ed333002e0b95e493c31991a683
Dawn Felstar <thelegotechhead@yahoo.com.au>
2021-01-16 05:44:20 +1100

    Honey generators now use TER to show their honey level

---
+++ src/main/java/com/resourcefulbees/resourcefulbees/client/render/tileentity/RenderHoneyGenerator.java
@@ -0,0 +1,38 @@
+package com.resourcefulbees.resourcefulbees.client.render.tileentity;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.resourcefulbees.resourcefulbees.tileentity.HoneyGeneratorTileEntity;
+import com.resourcefulbees.resourcefulbees.utils.CubeModel;
+import com.resourcefulbees.resourcefulbees.utils.RenderCuboid;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraftforge.fluids.FluidStack;
+
+public class RenderHoneyGenerator extends TileEntityRenderer<HoneyGeneratorTileEntity> {
+
+    public RenderHoneyGenerator(TileEntityRendererDispatcher renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public void render(HoneyGeneratorTileEntity tile, float partialTick, MatrixStack matrix, IRenderTypeBuffer renderer, int light, int overlayLight) {
+        if (tile.getWorld() == null) return;
+        FluidStack stack = tile.fluidTank.getFluid();
+        if (stack != null && !stack.isEmpty()) {
+            int level = tile.getLevel();
+            int color = stack.getFluid().getAttributes().getColor();
+            ResourceLocation stillTexture = stack.getFluid().getAttributes().getStillTexture();
+            IVertexBuilder builder = renderer.getBuffer(RenderType.getTranslucent());
+            Vector3f start = new Vector3f(0.0625f, 0.0625f, 0.0625f);
+            Vector3f end = new Vector3f(0.9375f, 0.0625f + ((float) level / 100.0F) * 0.875f, 0.9375f);
+            CubeModel model = new CubeModel(start, end);
+            model.setTextures(stillTexture);
+            RenderCuboid.INSTANCE.renderCube(model, matrix, builder, color, light, overlayLight);
+        }
+    }
+}
