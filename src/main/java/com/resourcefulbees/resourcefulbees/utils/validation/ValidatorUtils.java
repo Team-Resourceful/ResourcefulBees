0dac6bb9982e3c8c844932f65a006946fbb1f127
bkellkell <b.kelly497@gmail.com>
2021-01-18 23:43:53 -0500

    Update src/main/java/com/resourcefulbees/resourcefulbees/utils/validation/ValidatorUtils.java
    Co-authored-by: BlueAgent <BlueAgent@users.noreply.github.com>

--- src/main/java/com/resourcefulbees/resourcefulbees/utils/validation/ValidatorUtils.java
+++ src/main/java/com/resourcefulbees/resourcefulbees/utils/validation/ValidatorUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import static com.resourcefulbees.resourcefulbees.ResourcefulBees.LOGGER;
 
 public class ValidatorUtils {
-    public static final Pattern SINGLE_RESOURCE_PATTERN = Pattern.compile("^([\\w,\\-,\\.]+):([\\w,\\-,\\.,\\/]+)$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern SINGLE_RESOURCE_PATTERN = Pattern.compile("^([\\w\\-\\.]+):([\\w\\-\\.\\/]+)$", Pattern.CASE_INSENSITIVE);
     public static final Pattern TAG_RESOURCE_PATTERN = Pattern.compile("^(tag:)(\\w+):(\\w+/\\w+|\\w+)$", Pattern.CASE_INSENSITIVE);
     public static final Pattern ENTITY_RESOURCE_PATTERN = Pattern.compile("^(entity:)(\\w+):(\\w+/\\w+|\\w+)$", Pattern.CASE_INSENSITIVE);
 
