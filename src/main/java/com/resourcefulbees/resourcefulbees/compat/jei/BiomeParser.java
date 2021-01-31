e4741524ee3dbeb0e1d9dde8d5a2c20defd16634
Dawn Felstar <thelegotechhead@yahoo.com.au>
2021-02-01 07:27:44 +1100

    biome list in JEI now populates properly when using forge types

--- src/main/java/com/resourcefulbees/resourcefulbees/compat/jei/BiomeParser.java
+++ src/main/java/com/resourcefulbees/resourcefulbees/compat/jei/BiomeParser.java
@@ -2,14 +2,16 @@ package com.resourcefulbees.resourcefulbees.compat.jei;
 
 import com.google.common.base.Splitter;
 import com.resourcefulbees.resourcefulbees.api.beedata.CustomBeeData;
+import com.resourcefulbees.resourcefulbees.config.Config;
 import com.resourcefulbees.resourcefulbees.lib.BeeConstants;
 import com.resourcefulbees.resourcefulbees.registry.BiomeDictionary;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
 import org.apache.commons.lang3.text.WordUtils;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BiomeParser {
 
@@ -36,14 +38,39 @@ public class BiomeParser {
 
     private static Set<ResourceLocation> parseBiomeListFromTag(String list) {
         Set<ResourceLocation> biomeSet = new HashSet<>();
-        Splitter.on(",").trimResults().split(list.replace(BeeConstants.TAG_PREFIX,"")).forEach(s -> {
-            if (BiomeDictionary.TYPES.containsKey(s)) {
-                biomeSet.addAll(BiomeDictionary.TYPES.get(s));
+        Splitter.on(",").trimResults().split(list.replace(BeeConstants.TAG_PREFIX, "")).forEach(s -> {
+            if (Config.USE_FORGE_DICTIONARIES.get()) {
+                net.minecraftforge.common.BiomeDictionary.Type type = getForgeType(s);
+                if (type != null) {
+                    biomeSet.addAll(getForgeBiomeLocations(type));
+                }
+            } else {
+                if (BiomeDictionary.TYPES.containsKey(s)) {
+                    biomeSet.addAll(BiomeDictionary.TYPES.get(s));
+                }
             }
         });
-
         return biomeSet;
     }
+
+    private static Set<RegistryKey<Biome>> getForgeBiomes(net.minecraftforge.common.BiomeDictionary.Type type) {
+        return net.minecraftforge.common.BiomeDictionary.getBiomes(type);
+    }
+
+    private static Collection<? extends ResourceLocation> getForgeBiomeLocations(net.minecraftforge.common.BiomeDictionary.Type type) {
+        return getForgeBiomes(type).stream().map(b -> b.getValue()).collect(Collectors.toList());
+    }
+
+    private static net.minecraftforge.common.BiomeDictionary.Type getForgeType(String s) {
+        Collection<net.minecraftforge.common.BiomeDictionary.Type> forgeDict = net.minecraftforge.common.BiomeDictionary.Type.getAll();
+        for (net.minecraftforge.common.BiomeDictionary.Type type : forgeDict) {
+            if (type.getName().toLowerCase().equals(s)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
 
     private static Set<ResourceLocation> parseBiomeList(String list) {
         Set<ResourceLocation> biomeSet = new HashSet<>();
@@ -56,7 +83,7 @@ public class BiomeParser {
         StringJoiner returnList = new StringJoiner(", ");
         whitelist.stream()
                 .filter(resourceLocation -> !blacklist.contains(resourceLocation))
-                .forEach(resourceLocation -> returnList.add(WordUtils.capitalize(resourceLocation.getPath().replaceAll("_"," "))));
+                .forEach(resourceLocation -> returnList.add(WordUtils.capitalize(resourceLocation.getPath().replaceAll("_", " "))));
         return returnList.toString();
     }
 }
