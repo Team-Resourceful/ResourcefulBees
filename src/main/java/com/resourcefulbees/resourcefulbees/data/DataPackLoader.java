861eaaa09a7f96ffec7930998e04a1011eecbf62
ThatGravyBoat <gravyboat211@gmail.com>
2021-03-09 14:22:29 -0330

    Changed how internal datapack gets loaded

--- src/main/java/com/resourcefulbees/resourcefulbees/data/DataPackLoader.java
+++ src/main/java/com/resourcefulbees/resourcefulbees/data/DataPackLoader.java
@@ -1,58 +1,32 @@
 package com.resourcefulbees.resourcefulbees.data;
 
-import com.google.common.collect.Lists;
 import com.resourcefulbees.resourcefulbees.init.BeeSetup;
-import com.resourcefulbees.resourcefulbees.lib.ModConstants;
 import net.minecraft.resources.FolderPack;
+import net.minecraft.resources.IPackFinder;
 import net.minecraft.resources.IPackNameDecorator;
 import net.minecraft.resources.ResourcePackInfo;
-import net.minecraft.resources.ResourcePackList;
-import net.minecraft.world.storage.IServerConfiguration;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.Collection;
+import java.util.function.Consumer;
 
-public class DataPackLoader {
+public class DataPackLoader implements IPackFinder {
 
-    private DataPackLoader() {
-        throw new IllegalStateException(ModConstants.UTILITY_CLASS);
-    }
+    public static final DataPackLoader INSTANCE = new DataPackLoader();
 
-    public static void serverStarting(FMLServerAboutToStartEvent event) {
-        event.getServer().getResourcePacks().addPackFinder((packInfoConsumer, factory) -> {
-            File configDataPackFile = BeeSetup.getResourcePath().toFile();
-            if(configDataPackFile.exists() && configDataPackFile.isDirectory()) {
-                ResourcePackInfo pack = ResourcePackInfo.createResourcePack(
-                        "resourcefulbees:internals",
-                        true,
-                        () -> new FolderPack(BeeSetup.getResourcePath().toFile()),
-                        factory,
-                        ResourcePackInfo.Priority.BOTTOM,
-                        IPackNameDecorator.method_29485()
-                );
-                if (pack != null) packInfoConsumer.accept(pack);
-            }
-        });
-
-
-
-        ResourcePackList packs = event.getServer().getResourcePacks();
-        packs.reloadPacksFromFinders();
-
-        event.getServer().reloadResources(getDataPacks(packs, event.getServer().getSaveProperties(), packs.getEnabledNames())).exceptionally(exception -> null);
-    }
-
-    private static Collection<String> getDataPacks(ResourcePackList packs, IServerConfiguration serverConf, Collection<String> existingPacks) {
-        Collection<String> collection = Lists.newArrayList(existingPacks);
-        Collection<String> collection1 = serverConf.getDataPackSettings().getDisabled();
-
-        for (String s : packs.getNames()) {
-            if (!collection1.contains(s) && !collection.contains(s)) {
-                collection.add(s);
-            }
+    @Override
+    public void register(@NotNull Consumer<ResourcePackInfo> packList, ResourcePackInfo.@NotNull IFactory factory) {
+        File configDataPackFile = BeeSetup.getResourcePath().toFile();
+        if(configDataPackFile.exists() && configDataPackFile.isDirectory()) {
+            ResourcePackInfo pack = ResourcePackInfo.createResourcePack(
+                    "resourcefulbees:internals",
+                    true,
+                    () -> new FolderPack(BeeSetup.getResourcePath().toFile()),
+                    factory,
+                    ResourcePackInfo.Priority.BOTTOM,
+                    IPackNameDecorator.PACK_SOURCE_BUILTIN
+            );
+            if (pack != null) packList.accept(pack);
         }
-
-        return collection;
     }
 }
