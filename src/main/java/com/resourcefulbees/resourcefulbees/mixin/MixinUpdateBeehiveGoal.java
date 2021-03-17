f7e90d1720792a6a1db09ae3e66d3b3065965cc2
James <th3darkrose@gmail.com>
2021-03-17 15:57:17 -0400

    changed this$0 to a @Unique for bee goals update version

--- src/main/java/com/resourcefulbees/resourcefulbees/mixin/MixinUpdateBeehiveGoal.java
+++ src/main/java/com/resourcefulbees/resourcefulbees/mixin/MixinUpdateBeehiveGoal.java
@@ -7,12 +7,10 @@ import net.minecraft.village.PointOfInterest;
 import net.minecraft.village.PointOfInterestManager;
 import net.minecraft.village.PointOfInterestType;
 import net.minecraft.world.server.ServerWorld;
-import org.spongepowered.asm.mixin.Dynamic;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Comparator;
@@ -23,20 +21,24 @@ import java.util.stream.Stream;
 @Mixin(BeeEntity.UpdateBeehiveGoal.class)
 public abstract class MixinUpdateBeehiveGoal {
 
-    @Dynamic
-    @Final
-    @Shadow(aliases = {"BeeEntity$EnterBeehiveGoal.this$0", "this$0", "..."})
-    private BeeEntity this$0;
+    @Unique
+    private BeeEntity beeEntity;
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "<init>(Lnet/minecraft/entity/passive/BeeEntity;)V", at = @At(value = "RETURN"))
+    private void init(BeeEntity beeEntity, CallbackInfo ci) {
+        this.beeEntity = beeEntity;
+    }
 
     @Inject(at = @At(value = "HEAD"), method = "findNearbyHivesWithSpace()Ljava/util/List;", cancellable = true)
     public void findNearbyHivesWithSpace(CallbackInfoReturnable<List<BlockPos>> cir) {
-        BlockPos blockpos = this$0.blockPosition();
-        PointOfInterestManager pointofinterestmanager = ((ServerWorld) this$0.level).getPoiManager();
+        BlockPos blockpos = beeEntity.blockPosition();
+        PointOfInterestManager pointofinterestmanager = ((ServerWorld) beeEntity.level).getPoiManager();
         Stream<PointOfInterest> stream = pointofinterestmanager.getInRange(pointOfInterestType -> pointOfInterestType == PointOfInterestType.BEEHIVE
                 || pointOfInterestType == PointOfInterestType.BEE_NEST
                 || pointOfInterestType == ModPOIs.TIERED_BEEHIVE_POI.get(), blockpos, 20, PointOfInterestManager.Status.ANY);
         cir.setReturnValue(stream.map(PointOfInterest::getPos)
-                .filter(((BeeEntityAccessor) this$0)::callDoesHiveHaveSpace)
+                .filter(((BeeEntityAccessor) beeEntity)::callDoesHiveHaveSpace)
                 .sorted(Comparator.comparingDouble(blockPos -> blockPos.distSqr(blockpos)))
                 .collect(Collectors.toList()));
     }
