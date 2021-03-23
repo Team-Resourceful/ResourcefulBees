e1f96e60bef0daeedd5b99693ea733499caa3904
ThatGravyBoat <gravyboat211@gmail.com>
2021-03-23 09:45:29 -0230

    Fixed Mixin not loading correctly, can now load

--- forge/src/main/java/com/resourcefulbees/resourcefulbees/mixin/MixinEnterBeehiveGoal.java
+++ forge/src/main/java/com/resourcefulbees/resourcefulbees/mixin/MixinEnterBeehiveGoal.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Bee.BeeLocateHiveGoal.class)
+@Mixin(Bee.BeeEnterHiveGoal.class)
 public class MixinEnterBeehiveGoal {
 
     /*@Dynamic
