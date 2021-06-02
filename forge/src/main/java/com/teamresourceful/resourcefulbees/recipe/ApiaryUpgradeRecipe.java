108b9a77e4b9056e4aa35f8030885b7f437942b3
ThatGravyBoat <gravyboat211@gmail.com>
2021-06-02 17:17:51 -0230

    Fixed UpgradeRecipe Handler and made it so it returns 1 bee box per hive in a recipe.

--- forge/src/main/java/com/teamresourceful/resourcefulbees/recipe/ApiaryUpgradeRecipe.java
+++ forge/src/main/java/com/teamresourceful/resourcefulbees/recipe/ApiaryUpgradeRecipe.java
@@ -6,69 +6,50 @@ import com.teamresourceful.resourcefulbees.lib.constants.NBTConstants;
 import com.teamresourceful.resourcefulbees.registry.ModItems;
 import com.teamresourceful.resourcefulbees.registry.ModRecipeSerializers;
 import net.minecraft.core.NonNullList;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ApiaryUpgradeRecipe extends ShapedRecipe {
 
-    public ApiaryUpgradeRecipe(ResourceLocation id, NonNullList<Ingredient> ingredients, ItemStack result) {
-        super(id, "", 3, 3, ingredients, result);
+    public ApiaryUpgradeRecipe(ResourceLocation id, String group, int width, int height, NonNullList<Ingredient> ingredients, ItemStack result) {
+        super(id, group, width, height, ingredients, result);
     }
 
     @Override
     public @NotNull NonNullList<ItemStack> getRemainingItems(@NotNull CraftingContainer inventory) {
-        List<ItemStack> stacks = getHives(inventory);
         NonNullList<ItemStack> remainingItems = super.getRemainingItems(inventory);
-        ItemStack beeBox = new ItemStack(ModItems.CRAFTING_BEE_BOX.get());
-        CompoundTag tag = new CompoundTag();
-        ListTag allBees = new ListTag();
-        for (ItemStack stack : stacks) {
-            if (!stack.hasTag() || stack.getTag() == null || stack.getTag().isEmpty() || !stack.getTag().contains("BlockEntityTag"))
-                return remainingItems;
-            ListTag list = stack.getTag().getCompound("BlockEntityTag").getList(NBTConstants.NBT_BEES, 10);
-            if (!list.isEmpty()) {
-                allBees.addAll(list);
-            }
-        }
-        if (!allBees.isEmpty()) {
-            tag.put(NBTConstants.NBT_BEES, allBees);
-            beeBox.setTag(tag);
-            for (int i = 0; i < remainingItems.size(); i++) {
-                if (remainingItems.get(i).getItem() == Items.AIR) {
-                    remainingItems.set(i, beeBox);
-                    break;
-                }
-            }
-        }
-        return remainingItems;
-    }
 
-    public List<ItemStack> getHives(CraftingContainer inventory) {
-        List<ItemStack> stacks = new ArrayList<>();
         for (int i = 0; i < inventory.getContainerSize(); i++) {
             ItemStack item = inventory.getItem(i);
             Block block = Block.byItem(item.getItem());
-            if (block == Blocks.AIR) continue;
             if (block instanceof ApiaryBlock || block instanceof BeehiveBlock) {
-                stacks.add(item);
+                ItemStack box = getBeeBoxFromHive(item);
+                if (box == null) continue;
+                remainingItems.set(i, box);
             }
         }
-        return stacks;
+
+        return remainingItems;
+    }
+
+    private ItemStack getBeeBoxFromHive(ItemStack hive){
+        if (!hive.hasTag() || hive.getTag() == null || hive.getTag().isEmpty() || !hive.getTag().contains("BlockEntityTag")) return null;
+        ListTag list = hive.getTag().getCompound("BlockEntityTag").getList(NBTConstants.NBT_BEES, 10);
+        if (!list.isEmpty()){
+            ItemStack box = ModItems.CRAFTING_BEE_BOX.get().getDefaultInstance();
+            box.getOrCreateTag().put(NBTConstants.NBT_BEES, list);
+            return box;
+        }
+        return null;
     }
 
     @Override
@@ -81,14 +62,14 @@ public class ApiaryUpgradeRecipe extends ShapedRecipe {
         @Override
         public @NotNull ShapedRecipe fromJson(@NotNull ResourceLocation recipeId, @NotNull JsonObject json) {
             ShapedRecipe recipe = super.fromJson(recipeId, json);
-            return new ApiaryUpgradeRecipe(recipeId, recipe.getIngredients(), recipe.getResultItem());
+            return new ApiaryUpgradeRecipe(recipeId, recipe.getGroup(), recipe.getWidth(), recipe.getHeight(), recipe.getIngredients(), recipe.getResultItem());
         }
 
         @Override
         public ShapedRecipe fromNetwork(@NotNull ResourceLocation recipeId, @NotNull FriendlyByteBuf buffer) {
             ShapedRecipe recipe = super.fromNetwork(recipeId, buffer);
             assert recipe != null : "why is recipe null?";
-            return new ApiaryUpgradeRecipe(recipeId, recipe.getIngredients(), recipe.getResultItem());
+            return new ApiaryUpgradeRecipe(recipeId, recipe.getGroup(), recipe.getWidth(), recipe.getHeight(), recipe.getIngredients(), recipe.getResultItem());
         }
     }
 }
