02b1031e1555e6e03ae2cd6f63c5ba0592f93634
ThatGravyBoat <thatgravyboat@gmail.com>
2021-07-27 16:30:33 -0230

    Moved from arch to forge only Added geckolib

--- forge/src/main/java/com/teamresourceful/resourcefulbees/tileentity/multiblocks/apiary/IApiaryMultiblock.java
+++
@@ -1,8 +0,0 @@
-package com.teamresourceful.resourcefulbees.tileentity.multiblocks.apiary;
-
-import com.teamresourceful.resourcefulbees.lib.enums.ApiaryTab;
-import net.minecraft.server.level.ServerPlayer;
-
-public interface IApiaryMultiblock{
-    void switchTab(ServerPlayer player, ApiaryTab tab);
-}
