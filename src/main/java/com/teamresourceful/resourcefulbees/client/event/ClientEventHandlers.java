48764831f703286afbab45a504ceb6f04356f2e1
ThatGravyBoat <thatgravyboat@gmail.com>
2021-09-03 05:51:41 -0230

    Moved ColorHandler to the client package as it makes more sense.

--- src/main/java/com/teamresourceful/resourcefulbees/client/event/ClientEventHandlers.java
+++ src/main/java/com/teamresourceful/resourcefulbees/client/event/ClientEventHandlers.java
@@ -15,7 +15,7 @@ import com.teamresourceful.resourcefulbees.common.registry.minecraft.ModBlockEnt
 import com.teamresourceful.resourcefulbees.common.registry.minecraft.ModBlocks;
 import com.teamresourceful.resourcefulbees.common.registry.minecraft.ModContainers;
 import com.teamresourceful.resourcefulbees.common.registry.minecraft.ModEntities;
-import com.teamresourceful.resourcefulbees.common.utils.color.ColorHandler;
+import com.teamresourceful.resourcefulbees.client.color.ColorHandler;
 import com.teamresourceful.resourcefulbees.common.lib.constants.ModConstants;
 import com.teamresourceful.resourcefulbees.common.utils.PreviewHandler;
 import net.minecraft.client.Minecraft;
