69a79fcb405e77f82b5a801509e22976e8ac943a
James <th3darkrose@gmail.com>
2022-10-04 03:30:31 -0400

    updated forge and all dependencies - also added create back in

--- src/main/java/com/teamresourceful/resourcefulbees/client/render/pet/PetBeeRenderer.java
+++ src/main/java/com/teamresourceful/resourcefulbees/client/render/pet/PetBeeRenderer.java
@@ -2,10 +2,22 @@ package com.teamresourceful.resourcefulbees.client.render.pet;
 
 import com.teamresourceful.resourcefulbees.client.pets.PetBeeModel;
 import com.teamresourceful.resourcefulbees.client.pets.PetModelData;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 public class PetBeeRenderer implements IGeoRenderer<PetModelData> {
+
+    @Override
+    public void setCurrentRTB(MultiBufferSource rtb) {
+        //TODO figure out what goes here!
+    }
+
+    @Override
+    public MultiBufferSource getCurrentRTB() {
+        //TODO figure out what goes here!
+        return null;
+    }
 
     @Override
     public PetBeeModel<PetModelData> getGeoModelProvider() {
