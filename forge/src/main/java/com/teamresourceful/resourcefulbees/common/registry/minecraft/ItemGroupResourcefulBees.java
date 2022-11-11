b197c7ea88633aebcd55edb78befda13491e6f15
ThatGravyBoat <thatgravyboat@gmail.com>
2022-11-11 15:53:34 -0330

    custom creative tab builder

--- forge/src/main/java/com/teamresourceful/resourcefulbees/common/registry/minecraft/ItemGroupResourcefulBees.java
+++ forge/src/main/java/com/teamresourceful/resourcefulbees/common/registry/minecraft/ItemGroupResourcefulBees.java
@@ -3,15 +3,11 @@ package com.teamresourceful.resourcefulbees.common.registry.minecraft;
 import com.teamresourceful.resourcefulbees.ResourcefulBees;
 import com.teamresourceful.resourcefulbees.common.lib.constants.ModConstants;
 import com.teamresourceful.resourcefulbees.common.lib.constants.NBTConstants;
-import net.minecraft.core.NonNullList;
+import com.teamresourceful.resourcefulbees.common.registry.api.creativetab.CreativeTabBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
 
 public final class ItemGroupResourcefulBees {
 
@@ -19,66 +15,30 @@ public final class ItemGroupResourcefulBees {
         throw new IllegalAccessError(ModConstants.UTILITY_CLASS);
     }
 
-    public static final CreativeModeTab RESOURCEFUL_BEES = new CreativeModeTab(ResourcefulBees.MOD_ID) {
-
-        @Override
-        @NotNull
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.OAK_BEE_NEST_ITEM.get());
-        }
-
-        @Override
-        public void fillItemList(@NotNull NonNullList<ItemStack> stacks) {
-            Item beepedia = ModItems.BEEPEDIA.get();
-            for(Item item : ForgeRegistries.ITEMS) {
-                item.fillItemCategory(this, stacks);
-                if (item.equals(beepedia)) {
-                    ItemStack creativeBeepedia = new ItemStack(ModItems.BEEPEDIA.get());
-                    creativeBeepedia.getOrCreateTag().putBoolean(NBTConstants.Beepedia.CREATIVE, true);
-                    stacks.add(creativeBeepedia);
-                }
+    public static final CreativeModeTab RESOURCEFUL_BEES = CreativeTabBuilder.of(new ResourceLocation(ResourcefulBees.MOD_ID, "resourcefulbees"))
+        .setIcon(() -> new ItemStack(ModItems.HONEY_DIPPER.get()))
+        .setListingFunction((item, items) -> {
+            if (item.equals(ModItems.BEEPEDIA.get())) {
+                ItemStack creativeBeepedia = new ItemStack(ModItems.BEEPEDIA.get());
+                creativeBeepedia.getOrCreateTag().putBoolean(NBTConstants.Beepedia.CREATIVE, true);
+                items.add(creativeBeepedia);
             }
-        }
-    };
+        })
+        .build();
 
-    public static final CreativeModeTab RESOURCEFUL_BEES_HIVES = new CreativeModeTab(ResourcefulBees.MOD_ID + ".hives") {
+    public static final CreativeModeTab RESOURCEFUL_BEES_HIVES = CreativeTabBuilder.of(new ResourceLocation(ResourcefulBees.MOD_ID, "hives"))
+        .setIcon(() -> new ItemStack(ModItems.OAK_BEE_NEST_ITEM.get()))
+        .build();
 
-        @Override
-        @NotNull
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.OAK_BEE_NEST_ITEM.get());
-        }
-    };
+    public static final CreativeModeTab RESOURCEFUL_BEES_HONEY = CreativeTabBuilder.of(new ResourceLocation(ResourcefulBees.MOD_ID, "honey"))
+            .setIcon(() -> new ItemStack(Items.HONEY_BOTTLE))
+            .build();
 
-    public static final CreativeModeTab RESOURCEFUL_BEES_HONEY = new CreativeModeTab(ResourcefulBees.MOD_ID + ".honey") {
+    public static final CreativeModeTab RESOURCEFUL_BEES_COMBS = CreativeTabBuilder.of(new ResourceLocation(ResourcefulBees.MOD_ID, "combs"))
+            .setIcon(() -> new ItemStack(Items.HONEYCOMB))
+            .build();
 
-        @Override
-        @NotNull
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(Items.HONEY_BOTTLE);
-        }
-    };
-
-    public static final CreativeModeTab RESOURCEFUL_BEES_COMBS = new CreativeModeTab(ResourcefulBees.MOD_ID + ".combs") {
-
-        @Override
-        @NotNull
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(Items.HONEYCOMB);
-        }
-    };
-
-    public static final CreativeModeTab RESOURCEFUL_BEES_BEES = new CreativeModeTab(ResourcefulBees.MOD_ID + ".bees") {
-
-        @Override
-        @NotNull
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(Items.BEE_SPAWN_EGG);
-        }
-    };
+    public static final CreativeModeTab RESOURCEFUL_BEES_BEES = CreativeTabBuilder.of(new ResourceLocation(ResourcefulBees.MOD_ID, "bees"))
+            .setIcon(() -> new ItemStack(Items.BEE_SPAWN_EGG))
+            .build();
 }
