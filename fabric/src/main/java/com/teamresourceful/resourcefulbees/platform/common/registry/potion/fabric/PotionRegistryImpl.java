26ecc1a9a9fc246c1438fbb017897e61ef98a212
ThatGravyBoat <thatgravyboat@gmail.com>
2022-12-23 16:04:57 -0330

    Updated to use fabric api new potion api

--- fabric/src/main/java/com/teamresourceful/resourcefulbees/platform/common/registry/potion/fabric/PotionRegistryImpl.java
+++ fabric/src/main/java/com/teamresourceful/resourcefulbees/platform/common/registry/potion/fabric/PotionRegistryImpl.java
@@ -1,27 +1,26 @@
 package com.teamresourceful.resourcefulbees.platform.common.registry.potion.fabric;
 
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionBrewing;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.NotNull;
 
-//TODO replace with this when this gets merged https://github.com/FabricMC/fabric/pull/2670
 public class PotionRegistryImpl {
 
     public static void registerItemRecipe(@NotNull Item from, @NotNull Ingredient ingredient, @NotNull Item to) {
-        if (!(from instanceof PotionItem)) {
+        if (!(from instanceof PotionItem fromPotion)) {
             throw new IllegalArgumentException("Expected a potion, got: " + Registry.ITEM.getKey(from));
-        } else if (!(to instanceof PotionItem)) {
+        } else if (!(to instanceof PotionItem toPotion)) {
             throw new IllegalArgumentException("Expected a potion, got: " + Registry.ITEM.getKey(to));
         } else {
-            PotionBrewing.CONTAINER_MIXES.add(new PotionBrewing.Mix<>(from, ingredient, to));
+            FabricBrewingRecipeRegistry.registerItemRecipe(fromPotion, ingredient, toPotion);
         }
     }
 
     public static void registerPotionRecipe(@NotNull Potion from, @NotNull Ingredient ingredient, @NotNull Potion to) {
-        PotionBrewing.POTION_MIXES.add(new PotionBrewing.Mix<>(from, ingredient, to));
+        FabricBrewingRecipeRegistry.registerPotionRecipe(from, ingredient, to);
     }
 }
