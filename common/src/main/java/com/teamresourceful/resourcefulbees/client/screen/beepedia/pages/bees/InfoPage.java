7ed1db47b908d7cafe634a287b91fa2555011040
James Moore <th3darkrose@gmail.com>
2023-02-03 04:43:56 -0500

    added message to info page

--- common/src/main/java/com/teamresourceful/resourcefulbees/client/screen/beepedia/pages/bees/InfoPage.java
+++ common/src/main/java/com/teamresourceful/resourcefulbees/client/screen/beepedia/pages/bees/InfoPage.java
@@ -1,7 +1,10 @@
 package com.teamresourceful.resourcefulbees.client.screen.beepedia.pages.bees;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
+import org.jetbrains.annotations.NotNull;
 
 public class InfoPage extends Screen {
 
@@ -9,5 +12,10 @@ public class InfoPage extends Screen {
         super(CommonComponents.EMPTY);
     }
 
-
+    @Override
+    public void render(@NotNull PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        Minecraft.getInstance().font.drawShadow(poseStack, "Join our Discord to leave", 10, 10, 0xffffff);
+        Minecraft.getInstance().font.drawShadow(poseStack, "suggestions about what should", 10, 20, 0xffffff);
+        Minecraft.getInstance().font.drawShadow(poseStack, "go here!", 10, 30, 0xffffff);
+    }
 }
