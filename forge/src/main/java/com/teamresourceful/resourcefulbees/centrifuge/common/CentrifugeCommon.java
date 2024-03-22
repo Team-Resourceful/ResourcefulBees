f33df32d8166857c6a45358c8e9f34faa41d7e95
James Moore <th3darkrose@gmail.com>
2024-03-22 12:32:18 -0400

    <log>fixes centrifuge common setup event not subscribed on server<\log>

--- forge/src/main/java/com/teamresourceful/resourcefulbees/centrifuge/common/CentrifugeCommon.java
+++ forge/src/main/java/com/teamresourceful/resourcefulbees/centrifuge/common/CentrifugeCommon.java
@@ -7,7 +7,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD, modid = ModConstants.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ModConstants.MOD_ID)
 public class CentrifugeCommon {
 
     @SubscribeEvent
